2506ba4f35410c685dedd862b1e5703848c7c6c5
Smit Patel <smitp94@gmail.com>
2018-04-25 09:28:11 -0700

    Weight Vector

--- perceplearn3.py
+++ perceplearn3.py
@@ -6,7 +6,7 @@ import numpy as np
 file_write = 'data/nbmodel.txt'
 
 records = []
-unique_words = set()
+unique_words = []
 stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "I", "I'd", "I'll", "I'm", "I've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 # Totals = {}
 
@@ -66,24 +66,45 @@ def nbmodel_write(words, len_unique):
 def percept():
     global records
     global unique_words
-    all_words = []
+    global stopwords
 
     # For getting all unique words
     for r in records:
-        all_words.extend(r.text)
-    unique_words = set(all_words)
-    unique_words = [w for w in unique_words if w not in stopwords] # To remove stopwords
-    # print(unique_words)
+        for w in r.text:
+            if w not in unique_words and w not in stopwords:
+                unique_words.append(w)
+
+    # unique_words = [w for w in unique_words if w not in stopwords] # To remove stopwords
+
     # Initializing
-    w = np.zeros(len(unique_words), dtype=float)
+    Weight = np.zeros(len(unique_words), dtype=float)
+    x_vector = np.zeros(len(unique_words), dtype=int)
 
-    b = np.zeros(2,dtype=float)
-    # print(b)
-    for iter in range(30):
-        for feature in range(unique_words):
-            # activation = w[feature]*x + b[0]
-            pass
+    Bias = np.zeros(2,dtype=float)
 
+    # for itr in range(30):
+    for doc in records:
+        if doc.pos_neg == "Pos":
+            y = 1
+        else:
+            y = -1
+        for w in doc.text:
+            if w not in stopwords:
+                index = unique_words.index(w)
+                x_vector[index] = 1
+        # print(x_vector)
+
+        activation = np.sum(np.multiply(Weight, x_vector)) + Bias[0]
+
+        if y*activation <= 0:
+            Weight = Weight + (y*x_vector)
+            Bias[0] += y
+
+        x_vector = np.zeros(len(unique_words), dtype=int)
+
+    print((len(unique_words)))
+    for i in range(len(unique_words)-8000):
+        print(unique_words[i], Weight[i])
 
 
 read_file()
